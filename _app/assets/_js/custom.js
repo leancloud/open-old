b0529ee4dac4ecd6eedd70d30b6de69b3d647737
Tunghsiao Liu <t@sparanoid.com>
2015-08-19 18:08:51 +0800

    feat(theme): update template for new AMSF structure

--- _app/assets/_js/custom.js
+++ _app/assets/_js/custom.js
@@ -1,1 +1,8 @@
 // Custom user scripts
+(function(l,e,a,n,o,p,){l['GoogleAnalyticsObject']=o;l[o]=l[o]||function(){
+(l[o].q=l[o].q||[]).push(arguments)},l[o].l=1*new Date();p=e.createElement(a),
+=e.getElementsByTagName(a)[0];p.async=1;p.src=n;.parentNode.insertBefore(p,)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-42629236-5', 'auto');
+ga('send', 'pageview');
