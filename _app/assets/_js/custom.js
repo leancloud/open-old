c1138bd555f0a1549064d72628e6859c2e88965c
Tunghsiao Liu <t@sparanoid.com>
2015-08-20 11:34:24 +0800

    fix(script): wrong GA code

--- _app/assets/_js/custom.js
+++ _app/assets/_js/custom.js
@@ -1,7 +1,7 @@
 // Custom user scripts
-(function(l,e,a,n,o,p,){l['GoogleAnalyticsObject']=o;l[o]=l[o]||function(){
-(l[o].q=l[o].q||[]).push(arguments)},l[o].l=1*new Date();p=e.createElement(a),
-=e.getElementsByTagName(a)[0];p.async=1;p.src=n;.parentNode.insertBefore(p,)
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-42629236-5', 'auto');
