03f3a6ddbe81ac7fbb37d3a4b469ba34ce1759c7
Tunghsiao Liu <t@sparanoid.com>
2015-09-09 14:46:25 +0800

    feat(script): remove client-side leading quotes fix

--- _app/assets/_js/custom.js
+++ _app/assets/_js/custom.js
@@ -1,15 +1,1 @@
 // Custom user scripts
-
-// Quote marks fix
-+function addMarginFix() {
-  var para = document.querySelectorAll("p, li");
-
-  for (var i = 0; i < para.length; i++) {
-    var firstLetter = para[i].innerHTML.trim().charAt(0);
-    var regex = /「|『|“/;
-
-    if (firstLetter.match(regex)) {
-      para[i].className += ' ' + "leading-indent-fix";
-    }
-  }
-}();
